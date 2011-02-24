7505f928380d1694909644b8144508af6487c6a1
Alexander Potashev <aspotashev@gmail.com>
2011-02-24 22:45:41 +0300

    write dump to STL vector

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <assert.h>
 #include <string>
 #include <iostream>
+#include <vector>
 #include <gettext-po.h>
 
 void xerror_handler(
@@ -41,11 +42,11 @@ std::string wrap_string(const char *str)
 	return res;
 }
 
-std::string wrap_po_message(po_message_t message)
+std::pair<std::string, std::string> wrap_po_message(po_message_t message)
 {
 	if (*po_message_msgid(message) == '\0') // header
 	{
-		return "";
+		return make_pair(std::string(), std::string());
 	}
 
 	if (po_message_is_obsolete(message) || po_message_is_fuzzy(message))
@@ -54,24 +55,26 @@ std::string wrap_po_message(po_message_t message)
 		assert(0);
 	}
 
-	std::string res = wrap_string(po_message_msgid(message)); // msgid
+	std::string res1 = wrap_string(po_message_msgid(message)); // msgid
 	if (po_message_msgid_plural(message))
 	{
-		res += "_";
-		res += wrap_string(po_message_msgid_plural(message));
+		res1 += "_";
+		res1 += wrap_string(po_message_msgid_plural(message));
 	}
 
-	res += " ";
+	std::string res2;
 	if (po_message_msgstr_plural(message, 0)) // message has plural forms
-		res += po_message_msgstr_plural(message, 0);
+		res2 = po_message_msgstr_plural(message, 0);
 	else // single msgstr
-		res += po_message_msgstr(message);
+		res2 = po_message_msgstr(message);
 
-	return res;
+	return make_pair(res1, res2);
 }
 
-void dump_po_file_ids(const char *filename)
+std::vector<std::pair<std::string, std::string> > dump_po_file_ids(const char *filename)
 {
+	std::vector<std::pair<std::string, std::string> > res;
+
 	struct po_xerror_handler xerror_handlers;
 	xerror_handlers.xerror = xerror_handler;
 	xerror_handlers.xerror2 = xerror2_handler;
@@ -92,17 +95,22 @@ void dump_po_file_ids(const char *filename)
 	po_message_t message; // in fact, this is a pointer
 	for (int index = 0; message = po_next_message(iterator); index ++)
 	{
-		std::string msg_dump = wrap_po_message(message);
-		if (msg_dump.length() > 0)
-			std::cout << msg_dump << std::endl;
+		std::pair<std::string, std::string> msg_dump = wrap_po_message(message);
+		if (msg_dump.first.length() > 0)
+			res.push_back(msg_dump);
 	}
 
 	po_file_free(file); // free memory
+	return res;
 }
 
 int main()
 {
-	dump_po_file_ids("temp/dolphin.pot");
+	std::vector<std::pair<std::string, std::string> > dump = dump_po_file_ids("temp/dolphin.pot");
+	for (size_t i = 0; i < dump.size(); i ++)
+	{
+		std::cout << dump[i].first << " " << dump[i].second << std::endl;
+	}
 
 	return 0;
 }
