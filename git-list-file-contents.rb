cc5dc1869d4ef79e0081dc259f7aa3da5d5bcce3
Alexander Potashev <aspotashev@gmail.com>
2011-02-27 21:54:47 +0300

    cleanup

--- git-list-file-contents.rb
+++ git-list-file-contents.rb
@@ -1,22 +1,19 @@
 #!/usr/bin/ruby
 
-def git_list_of_file_contents(dir)
-  res = `cd #{dir} ; (for n in $(git log --format=format:%T) ; do git ls-tree -r --full-tree $n ; done) | sort | uniq`
-  res = res.split("\n").
+def lstree_output_to_pairs(input)
+  input.split("\n").
     map {|x| x.match(/^[0-9]{6} blob ([0-9a-f]{40})\t(.+)$/) }.
     select {|x| not x.nil? }.
     map {|m| [m[1], m[2]] }.
     uniq.
     map {|x| [x[0], x[1].match(/(^|\/)([^\/]+)$/)[2]] }.
     uniq
-
-## This is OK and often happens. For example, calligra/words.pot and koffice/kword.pot.
-#  if res.size != res.map {|x| x[0] }.uniq.size
-#    raise "Files with the same SHA-1s have different names."
-#  end
-
-  res
 end
 
-p git_list_of_file_contents('.')
+def git_list_of_file_contents(dir)
+  res = `cd #{dir} ; (for n in $(git log --format=format:%T) ; do git ls-tree -r --full-tree $n ; done) | sort | uniq`
+  lstree_output_to_pairs(res)
+end
+
+p git_list_of_file_contents('~/kde-ru/xx-numbering/templates')
 
