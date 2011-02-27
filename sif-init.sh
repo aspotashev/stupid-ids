179facac5a6307dfe5b6a08cb92af6a581f8100c
Alexander Potashev <aspotashev@gmail.com>
2011-02-27 14:33:34 +0300

    init

---
+++ sif-init.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+DIR=$1
+
+if [ -z "$DIR" ]; then
+	echo "Usage: ./sif-init.sh <dir>"
+	exit
+fi
+
+mkdir -p "$DIR"
+cd "$DIR"
+git init
+
+printf "100" > next_id.txt
+touch hash_map.txt
+git add next_id.txt hash_map.txt
+git commit -m "init"
+
