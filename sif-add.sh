d595f1e55dc3da06ab170e082ef340e6b7899a30
Alexander Potashev <aspotashev@gmail.com>
2011-02-27 19:53:05 +0300

    save all names of a .pot file

--- sif-add.sh
+++ sif-add.sh
@@ -27,12 +27,18 @@ if [ -z "$(grep --fixed-strings $POT_HASH hash_map.txt)" ]; then
 	NEXT_ID2=$(posieve.py fill_xx_numbering ./.file.po -snext_id:$NEXT_ID --quiet)
 	POIDS_HASH=`git-hash-object .file.po`
 
-	echo "$POT_HASH $POIDS_HASH $POT_NAME" >> hash_map.txt
+	echo "$POT_HASH $POIDS_HASH" >> hash_map.txt
 	printf $NEXT_ID2 > next_id.txt
 
 	git add .file.po hash_map.txt next_id.txt
-	git commit -m "new .pot: $POT_NAME"
 else
 	echo "The .pot file already exists in this repository."
 fi
+
+if [ -z "$(grep --fixed-strings "$POT_HASH $POT_NAME" pot_names.txt | grep "^$POT_HASH $POT_NAME$" pot_names.txt)" ]; then
+	echo "$POT_HASH $POT_NAME" >> pot_names.txt
+	git add pot_names.txt
+fi
+
+git commit -m "new .pot: $POT_NAME"
 
--- sif-init.sh
+++ sif-init.sh
@@ -13,6 +13,7 @@ git init
 
 printf "100" > next_id.txt
 touch hash_map.txt
-git add next_id.txt hash_map.txt
+touch pot_names.txt
+git add next_id.txt hash_map.txt pot_names.txt
 git commit -m "init"
 
