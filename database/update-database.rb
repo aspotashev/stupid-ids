2b5f60092d78ddf7c7ec5cfbadf0b0b6c91ac9e3
Alexander Potashev <aspotashev@gmail.com>
2011-03-06 12:06:09 +0300

    LastSha1 -> FillerLastSha1

--- database/update-database.rb
+++ database/update-database.rb
@@ -10,7 +10,7 @@ end
 
 class CreateDb < ActiveRecord::Migration
 	def self.up
-		create_table LastSha1.table_name do |t|
+		create_table FillerLastSha1.table_name do |t|
 			t.string :value
 		end
 
@@ -28,11 +28,11 @@ class CreateDb < ActiveRecord::Migration
 		end
 		add_index PotshaFirstId.table_name, [:potsha]
 
-		LastSha1.create(:id => 1, :value => 'init').save! # Git tag "init"
+		FillerLastSha1.create(:id => 1, :value => 'init').save! # Git tag "init"
 	end
 
 	def self.down
-		[LastSha1, NamedatePotsha, PotshaFirstId].each do |c|
+		[FillerLastSha1, NamedatePotsha, PotshaFirstId].each do |c|
 			drop_table c.table_name if table_exists?(c.table_name)
 		end
 	end
@@ -47,7 +47,7 @@ class PotshaFirstId < ActiveRecord::Base
 end
 
 # last_sha1.value = sha1 of the last processed Git commit (string)
-class LastSha1 < ActiveRecord::Base
+class FillerLastSha1 < ActiveRecord::Base
 	establish_connection_filler
 
 	def self.value
@@ -61,7 +61,7 @@ class LastSha1 < ActiveRecord::Base
 	end
 end
 
-if not LastSha1.table_exists?
+if not FillerLastSha1.table_exists?
 	CreateDb.migrate(:down)
 	CreateDb.migrate(:up)
 end
@@ -123,18 +123,18 @@ end
 
 $NEW_SHA1 = git_head_sha1 # updating to this SHA-1
 
-git_diff_lines(LastSha1.value, $NEW_SHA1, 'pot_names.txt').added.each do |x|
+git_diff_lines(FillerLastSha1.value, $NEW_SHA1, 'pot_names.txt').added.each do |x|
 	m = x.match(/^([0-9a-f]{40}) ([^ ]+) <(.+)>$/) or raise "failed to parse"
 	NamedatePotsha.create(:potsha => m[1], :potname => m[2], :potdate => m[3])
 end
 
-git_diff_lines(LastSha1.value, $NEW_SHA1, 'first_ids.txt').added.each do |x|
+git_diff_lines(FillerLastSha1.value, $NEW_SHA1, 'first_ids.txt').added.each do |x|
 	m = x.match(/^([0-9a-f]{40}) ([^ ]+)$/) or raise "failed to parse"
 	PotshaFirstId.create(:potsha => m[1], :first_id => m[2])
 end
 
 
-LastSha1.value = $NEW_SHA1
+FillerLastSha1.value = $NEW_SHA1
 
 
 p NamedatePotsha.find(:all)
