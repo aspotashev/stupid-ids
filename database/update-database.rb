cf4206ba1350b76dba3b4363e8179257e1dd502c
Alexander Potashev <aspotashev@gmail.com>
2011-03-06 14:13:22 +0300

    cleanup

--- database/update-database.rb
+++ database/update-database.rb
@@ -8,6 +8,31 @@ ActiveRecord::Base.class_eval do
 	end
 end
 
+class FillerBase < ActiveRecord::Base
+	# http://pragdave.blogs.pragprog.com/pragdave/2006/01/sharing_externa.html
+	self.abstract_class = true
+	establish_connection_filler
+end
+
+class NamedatePotsha < FillerBase
+end
+
+class PotshaFirstId < FillerBase
+end
+
+# last_sha1.value = sha1 of the last processed Git commit (string)
+class FillerLastSha1 < FillerBase
+	def self.value
+		find(:first).value
+	end
+
+	def self.value=(new_value)
+		x = find(:first)
+		x.value = new_value
+		x.save!
+	end
+end
+
 class CreateDb < ActiveRecord::Migration
 	def self.connection
 		FillerBase.connection
@@ -39,31 +64,6 @@ class CreateDb < ActiveRecord::Migration
 		[FillerLastSha1, NamedatePotsha, PotshaFirstId].each do |c|
 			drop_table c.table_name if table_exists?(c.table_name)
 		end
-	end
-end
-
-class FillerBase < ActiveRecord::Base
-	# http://pragdave.blogs.pragprog.com/pragdave/2006/01/sharing_externa.html
-	self.abstract_class = true
-	establish_connection_filler
-end
-
-class NamedatePotsha < FillerBase
-end
-
-class PotshaFirstId < FillerBase
-end
-
-# last_sha1.value = sha1 of the last processed Git commit (string)
-class FillerLastSha1 < FillerBase
-	def self.value
-		find(:first).value
-	end
-
-	def self.value=(new_value)
-		x = find(:first)
-		x.value = new_value
-		x.save!
 	end
 end
 
