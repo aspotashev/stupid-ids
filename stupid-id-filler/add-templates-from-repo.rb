e2ff5bbb6eae2a9da116c575e9bfcd3079c30b45
Alexander Potashev <aspotashev@gmail.com>
2011-03-20 14:02:56 +0300

    cleanup

--- stupid-id-filler/add-templates-from-repo.rb
+++ stupid-id-filler/add-templates-from-repo.rb
@@ -5,22 +5,6 @@ require './sif-lib.rb'
 $SRC_DIR = '~/kde-ru/xx-numbering/templates'
 $IDS_DIR = './ids'
 
-#def lstree_output_to_pairs(input)
-#  input.split("\n").
-#    map {|x| x.match(/^[0-9]{6} blob ([0-9a-f]{40})\t(.+)$/) }.
-#    select {|x| not x.nil? }.
-#    map {|m| [m[1], m[2]] }.
-#    uniq.
-#    map {|x| [x[0], x[1].match(/(^|\/)([^\/]+)\.pot$/)[2]] }.
-#    uniq
-#end
-
-def git_list_of_file_contents
-  res = `cd #{$SRC_DIR} ; (for n in $(git log --format=format:%T) ; do git ls-tree -r --full-tree $n ; done) | sort | uniq`
-  lstree_output_to_pairs(res)
-end
-
-#p git_list_of_file_contents
 
 def list_of_processed
 	f = begin
