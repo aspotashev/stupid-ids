e7ba6b8e31c6ac116773f700eec1806c9a89e94c
Alexander Potashev <aspotashev@gmail.com>
2011-04-06 20:16:22 +0400

    check for empty 'msgstr's in POT

--- stupid-id-filler/check-lib.rb
+++ stupid-id-filler/check-lib.rb
@@ -1,9 +1,26 @@
 
+Array.class_eval do
+	def neighbor_pairs
+		(0..(size-2)).map {|i| [self[i], self[i+1]] }
+	end
+end
+
 def is_virgin_pot_content(content)
 	lines = content.split("\n")
 
 	if lines.any? {|line| line.match(/^#\. \+> /) }
 		return false # files from PO Summit ( see http://techbase.kde.org/Localization/Workflows/PO_Summit )
+	end
+
+	if lines[lines.find_index("msgid \"\"") - 1] != "#, fuzzy"
+		return false # header of .POT should be fuzzy
+	end
+
+	if (lines + ['']).neighbor_pairs.
+		select {|pair| pair[0].match(/^msgstr(\[[0-9]+\])? /) }[1..-1].
+		any? {|pair| pair[0].sub(/^msgstr(\[[0-9]+\])? /, '') != "\"\"" or (pair[1] != "" and not pair[1].match(/^msgstr/)) }
+
+		return false # .POT contains translations in 'msgstr' fields
 	end
 
 	true # All tests passed
