848c316e843138012b08977625d6fe94d0b4f1ef
Alexander Potashev <aspotashev@gmail.com>
2011-04-13 21:17:27 +0400

    hack-fix creation of Makefile for gettextpo-helper/ruby-ext

--- gettextpo-helper/ruby-ext/extconf.rb
+++ gettextpo-helper/ruby-ext/extconf.rb
@@ -1,6 +1,11 @@
 
 # /* http://www.rubyinside.com/how-to-create-a-ruby-extension-in-c-in-under-5-minutes-100.html */
 require 'mkmf'
+
+def try_func(func, libs, headers = nil, &b) # FIXME (hack!)
+	true
+end
+
 extension_name = 'gettextpo_helper'
 dir_config(extension_name)
 have_library('cryptopp')
