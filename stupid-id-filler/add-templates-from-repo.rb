493718a7547b20f0d365f2295441c41fb353708a
Alexander Potashev <aspotashev@gmail.com>
2011-04-29 21:57:17 +0400

    add POTs to white_list

--- stupid-id-filler/add-templates-from-repo.rb
+++ stupid-id-filler/add-templates-from-repo.rb
@@ -77,7 +77,25 @@ commits_to_process.each_with_index do |commit_sha1, index|
 		if is_virgin_pot(tempfile_pot) == :ok
 			sif.add(tempfile_pot, :pot_hash => content_sha1)
 		else
-			white_list = [
+			white_list = [ # known really broken POTs
+				'c858f9fb3d0879344e020e8da6e75b49ec69f422', # POTs with translations
+				'367d7b7c18ba66a6d308a92401f79e96a8b50e0c',
+				'ed6cfadfaa021938da118044555e7f0112bb32cd',
+				'72358faf60b872e2113041a5ecaa1b0c8ae9bf73',
+				'4c1c3e3ff0c853256401dbc910cbc70050904cce',
+				'941f68ea0de6e37d3bbec96720ee4a5929b589a1',
+				'dfc6efaec41640efd65f08105b32d9cd3e5c6493',
+				'7102486a99154cf41d21ee8c29475bfc263c17fc',
+				'92fa2380247f1445123c79d1b0562bdb40758e26',
+				'7ead16f41b58e4e4b63473c8b84664d90ac79255',
+				'66767c4cfc5e349d78da99c9fa4fb88d9edd0a79',
+				'39794651da0335a6017f6b8d94a3e292fce106a5',
+				'f59b8f79171b52986d8b44e98117b398fafa8384',
+
+				'24c554289355b4b805ab1654fbe5c52ebe02f905', # header is not clean
+				'62e7afe093409b51fb688484e1f44ad3d6dfc336',
+
+				'b5fbe988b1355356528c10fd3c55041928a6afa6', # broken header
 			]
 
 			if is_virgin_pot(tempfile_pot) != :po_summit and not white_list.include?(content_sha1)
