1eed332f5ad52156ead4262a790e9f0fe3056d74
Alexander Potashev <aspotashev@gmail.com>
2011-05-24 01:04:14 +0400

    fix indentation

--- successor-detector/database/update-database.rb
+++ successor-detector/database/update-database.rb
@@ -3,13 +3,13 @@
 require 'active_record'
 
 ActiveRecord::Base.class_eval do
-	def self.establish_connection_filler
-		establish_connection(YAML::load(File.open('../../stupid-id-filler/database/filler-database.yml')))
-	end
+  def self.establish_connection_filler
+    establish_connection(YAML::load(File.open('../../stupid-id-filler/database/filler-database.yml')))
+  end
 
-	def self.establish_connection_merger
-		establish_connection(YAML::load(File.open('merger-database.yml')))
-	end
+  def self.establish_connection_merger
+    establish_connection(YAML::load(File.open('merger-database.yml')))
+  end
 end
 
 require 'stupidsruby'
@@ -18,30 +18,30 @@ require '../../stupid-id-filler/database/filler-models.rb'
 require '../../stupid-id-filler/check-lib.rb'
 
 Array.class_eval do
-	def is_uniq
-		self.uniq.size == size
-	end
+  def is_uniq
+    self.uniq.size == size
+  end
 end
 
 def get_pot_first_id(tp_hash)
-	TphashFirstId.find(:first, :conditions => {:tp_hash => tp_hash}).first_id
+  TphashFirstId.find(:first, :conditions => {:tp_hash => tp_hash}).first_id
 end
 
 $id_map_db = GettextpoHelper::IdMapDb.new('idmap.mmapdb')
 
 # Copied from stupid-ids/successor-detector/detect.rb
 def sha1_to_tp_hash(git_dir, sha1)
-	tempfile_pot = `tempfile --suffix=.pot`.strip
-	`cd "#{git_dir}" ; git show #{sha1} > "#{tempfile_pot}"`
+  tempfile_pot = `tempfile --suffix=.pot`.strip
+  `cd "#{git_dir}" ; git show #{sha1} > "#{tempfile_pot}"`
 
-	res = nil
-	if is_virgin_pot(tempfile_pot) == :ok
-		# TODO: GettextpoHelper.calculate_tp_hash should be able to read .pot from buffer (not only from a file)
-		res = GettextpoHelper.calculate_tp_hash(tempfile_pot)
-	end
+  res = nil
+  if is_virgin_pot(tempfile_pot) == :ok
+    # TODO: GettextpoHelper.calculate_tp_hash should be able to read .pot from buffer (not only from a file)
+    res = GettextpoHelper.calculate_tp_hash(tempfile_pot)
+  end
 
-	`rm -f "#{tempfile_pot}"`
-	res
+  `rm -f "#{tempfile_pot}"`
+  res
 end
 
 def oid_to_tp_hash(sha1)
@@ -49,46 +49,39 @@ def oid_to_tp_hash(sha1)
 end
 
 def update_database
-	pairs = GettextpoHelper.detect_transitions(
-		"/home/sasha/kde-ru/xx-numbering/templates/.git/",
-		"/home/sasha/kde-ru/xx-numbering/stable-templates/.git/",
-		"/home/sasha/l10n-kde4/scripts/process_orphans.txt")
-	pairs.each do |pair|
-		tp_hash_a = oid_to_tp_hash(pair[0])
-		tp_hash_b = oid_to_tp_hash(pair[1])
-		next if tp_hash_a == tp_hash_b
+  pairs = GettextpoHelper.detect_transitions(
+    "/home/sasha/kde-ru/xx-numbering/templates/.git/",
+    "/home/sasha/kde-ru/xx-numbering/stable-templates/.git/",
+    "/home/sasha/l10n-kde4/scripts/process_orphans.txt")
+  pairs.each do |pair|
+    tp_hash_a = oid_to_tp_hash(pair[0])
+    tp_hash_b = oid_to_tp_hash(pair[1])
+    next if tp_hash_a == tp_hash_b
 
-#		merge_pair_data = { :merge_file_id => merge_file.id, :tp_hash_a => tp_hash_a, :tp_hash_b => tp_hash_b }
-#		merge_pair = MergePair.find(:first, :conditions => merge_pair_data)
-#		if merge_pair.nil?
-#			merge_pair = MergePair.create(merge_pair_data)
-#			merge_pair.save
-#		end
+    tempfile_pot_a = `tempfile --suffix=.pot`.strip
+    tempfile_pot_b = `tempfile --suffix=.pot`.strip
 
-		tempfile_pot_a = `tempfile --suffix=.pot`.strip
-		tempfile_pot_b = `tempfile --suffix=.pot`.strip
+    extract_pot_to_file(tp_hash_a, tempfile_pot_a)
+    extract_pot_to_file(tp_hash_b, tempfile_pot_b)
 
-		extract_pot_to_file(tp_hash_a, tempfile_pot_a)
-		extract_pot_to_file(tp_hash_b, tempfile_pot_b)
+    # Results should _probably_ be cached (in files or in a database)
+    id_map_list = GettextpoHelper.list_equal_messages_ids_2(
+      tempfile_pot_a, get_pot_first_id(tp_hash_a),
+      tempfile_pot_b, get_pot_first_id(tp_hash_b))
 
-		# Results should _probably_ be cached (in files or in a database)
-		id_map_list = GettextpoHelper.list_equal_messages_ids_2(
-			tempfile_pot_a, get_pot_first_id(tp_hash_a),
-			tempfile_pot_b, get_pot_first_id(tp_hash_b))
+    `rm -f "#{tempfile_pot_a}"`
+    `rm -f "#{tempfile_pot_b}"`
 
-		`rm -f "#{tempfile_pot_a}"`
-		`rm -f "#{tempfile_pot_b}"`
+    puts "#{tp_hash_a} <-> #{tp_hash_b}: #{id_map_list.size} pairs of IDs"
 
-		puts "#{tp_hash_a} <-> #{tp_hash_b}: #{id_map_list.size} pairs of IDs"
+    id_map_list = id_map_list.
+      map {|id_pair| id_pair.sort }.
+      map {|id_pair| [id_pair[1], id_pair[0], 0] }
 
-		id_map_list = id_map_list.
-			map {|id_pair| id_pair.sort }.
-			map {|id_pair| [id_pair[1], id_pair[0], 0] }
-
-		raise if id_map_list.nil?
-		raise if $id_map_db.nil?
-		$id_map_db.create(id_map_list)
-	end
+    raise if id_map_list.nil?
+    raise if $id_map_db.nil?
+    $id_map_db.create(id_map_list)
+  end
 end
 
 update_database
