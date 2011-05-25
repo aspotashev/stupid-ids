4fc8afc4a3db6a9bec5ac04749de5cca1a852411
Alexander Potashev <aspotashev@gmail.com>
2011-05-25 22:03:56 +0400

    cleanup

--- transition-detector/update-database.rb
+++ transition-detector/update-database.rb
@@ -12,17 +12,11 @@ require 'stupidsruby'
 require '../gettextpo-helper/ruby-helpers/ruby-helpers'
 require '../stupid-id-filler/database/filler-models.rb'
 
-Array.class_eval do
-  def is_uniq
-    self.uniq.size == size
-  end
-end
-
 def get_pot_first_id(tp_hash)
   TphashFirstId.find(:first, :conditions => {:tp_hash => tp_hash}).first_id
 end
 
-$id_map_db = GettextpoHelper::IdMapDb.new('idmap.mmapdb')
+id_map_db = GettextpoHelper::IdMapDb.new('idmap.mmapdb')
 
 def oid_to_tp_hash(sha1)
   # TODO: create a database index to make this work faster
@@ -30,32 +24,30 @@ def oid_to_tp_hash(sha1)
   x.nil? ? nil : x.tp_hash
 end
 
-def update_database
-  git_loader = GettextpoHelper::GitLoader.new
-  git_loader.add_repository("/home/sasha/kde-ru/xx-numbering/templates/.git/")
-  git_loader.add_repository("/home/sasha/kde-ru/xx-numbering/stable-templates/.git/")
 
-  pairs = GettextpoHelper.detect_transitions(
-    "/home/sasha/kde-ru/xx-numbering/templates/.git/",
-    "/home/sasha/kde-ru/xx-numbering/stable-templates/.git/",
-    "/home/sasha/l10n-kde4/scripts/process_orphans.txt")
-  pairs.each do |pair|
-    tp_hash_a = oid_to_tp_hash(pair[0])
-    tp_hash_b = oid_to_tp_hash(pair[1])
-    next if tp_hash_a == tp_hash_b or tp_hash_a.nil? or tp_hash_b.nil?
+git_loader = GettextpoHelper::GitLoader.new
+git_loader.add_repository("/home/sasha/kde-ru/xx-numbering/templates/.git/")
+git_loader.add_repository("/home/sasha/kde-ru/xx-numbering/stable-templates/.git/")
+
+pairs = GettextpoHelper.detect_transitions(
+  "/home/sasha/kde-ru/xx-numbering/templates/.git/",
+  "/home/sasha/kde-ru/xx-numbering/stable-templates/.git/",
+  "/home/sasha/l10n-kde4/scripts/process_orphans.txt")
+pairs.each do |pair|
+  tp_hash_a = oid_to_tp_hash(pair[0])
+  tp_hash_b = oid_to_tp_hash(pair[1])
+  next if tp_hash_a == tp_hash_b or tp_hash_a.nil? or tp_hash_b.nil?
 
 
-    # Results should _probably_ be cached (in files or in a database)
-    n_pairs = GettextpoHelper.dump_equal_messages_to_mmapdb(
-      GettextpoHelper::TranslationContent.new(git_loader, pair[0]), get_pot_first_id(tp_hash_a),
-      GettextpoHelper::TranslationContent.new(git_loader, pair[1]), get_pot_first_id(tp_hash_b),
-      $id_map_db)
+  # Results should _probably_ be cached (in files or in a database)
+  n_pairs = GettextpoHelper.dump_equal_messages_to_mmapdb(
+    GettextpoHelper::TranslationContent.new(git_loader, pair[0]), get_pot_first_id(tp_hash_a),
+    GettextpoHelper::TranslationContent.new(git_loader, pair[1]), get_pot_first_id(tp_hash_b),
+    id_map_db)
 
-    puts "#{pair[0]} <-> #{pair[1]}: #{n_pairs} pairs of IDs"
-  end
+  puts "#{pair[0]} <-> #{pair[1]}: #{n_pairs} pairs of IDs"
 end
 
-update_database
 
-$id_map_db.normalize_database
+id_map_db.normalize_database
 
