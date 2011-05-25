cb1685f537784d6a145399069c728081fcd64a91
Alexander Potashev <aspotashev@gmail.com>
2011-05-25 17:19:51 +0400

    Fix 'dump-ids'

--- gettextpo-helper/dump-ids/main.cpp
+++ gettextpo-helper/dump-ids/main.cpp
@@ -8,13 +8,16 @@
 #include <vector>
 #include <map>
 
-#include <gettextpo-helper.h>
+#include <gettextpo-helper/gettextpo-helper.h>
+#include <gettextpo-helper/translationcontent.h>
 
 int main(int argc, char *argv[])
 {
 	assert(argc == 5); // 4 arguments
 
-	std::vector<std::pair<int, int> > list = list_equal_messages_ids_2(argv[1], atoi(argv[2]), argv[3], atoi(argv[4]));
+	std::vector<std::pair<int, int> > list = list_equal_messages_ids_2(
+		new TranslationContent(argv[1]), atoi(argv[2]),
+		new TranslationContent(argv[3]), atoi(argv[4]));
 	for (size_t i = 0; i < list.size(); i ++)
 		std::cout << list[i].first << " " << list[i].second << std::endl;
 
