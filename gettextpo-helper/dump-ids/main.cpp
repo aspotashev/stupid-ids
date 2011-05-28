99650bfbaefd3430539ab2982434def083ebc5b9
Alexander Potashev <aspotashev@gmail.com>
2011-05-28 14:13:03 +0400

    Fix memory leak

--- gettextpo-helper/dump-ids/main.cpp
+++ gettextpo-helper/dump-ids/main.cpp
@@ -15,11 +15,16 @@ int main(int argc, char *argv[])
 {
 	assert(argc == 5); // 4 arguments
 
+	TranslationContent *file_a = new TranslationContent(argv[1]);
+	TranslationContent *file_b = new TranslationContent(argv[3]);
 	std::vector<std::pair<int, int> > list = list_equal_messages_ids_2(
-		new TranslationContent(argv[1]), atoi(argv[2]),
-		new TranslationContent(argv[3]), atoi(argv[4]));
+		file_a, atoi(argv[2]),
+		file_b, atoi(argv[4]));
 	for (size_t i = 0; i < list.size(); i ++)
 		std::cout << list[i].first << " " << list[i].second << std::endl;
+
+	delete file_a;
+	delete file_b;
 
 	return 0;
 }
