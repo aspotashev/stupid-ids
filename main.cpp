ea711d1269431ee932eae5b442b38eff5842e900
Alexander Potashev <aspotashev@gmail.com>
2011-05-28 22:55:08 +0400

    respond to command "get_min_id_array"

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 
+#include <stdio.h>
 #include <string.h>
 
 #include "tcpcommandserver.h"
@@ -7,13 +8,31 @@ class Server : public TcpCommandServer
 {
 public:
 	virtual void commandHandler(const char *command);
+
+private:
+	static const char *getCommandArg(const char *input, const char *command);
 };
+
+const char *Server::getCommandArg(const char *input, const char *command)
+{
+	size_t cmd_len = strlen(command);
+	if (!strncmp(input, command, cmd_len) && input[cmd_len] == ' ')
+		return input + cmd_len + 1;
+	else
+		return NULL;
+}
 
 void Server::commandHandler(const char *command)
 {
+	const char *arg;
+
 	if (!strcmp(command, "exit"))
 	{
 		disconnect();
+	}
+	else if (arg = getCommandArg(command, "get_min_id_array"))
+	{
+		printf("tp_hash = %s\n", arg);
 	}
 	else
 	{
