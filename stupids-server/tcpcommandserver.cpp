62d795e3b8897611af2a8e0304d449bf00c42850
Alexander Potashev <aspotashev@gmail.com>
2011-05-29 16:47:41 +0400

    Fix

--- stupids-server/tcpcommandserver.cpp
+++ stupids-server/tcpcommandserver.cpp
@@ -36,7 +36,7 @@ void TcpCommandServer::sessionOpened()
 
 		int read_bytes = recvfrom(m_connfd, buffer + buffer_len, BUFFER_SZ - buffer_len,
 			0, (struct sockaddr *)&m_cliaddr, &m_clilen);
-		if (read_bytes < 0)
+		if (read_bytes <= 0)
 		{
 			printf("Client (probably) disconnected unexpectedly.\n");
 			break;
