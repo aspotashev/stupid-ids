474cf3b347adadcc7d837f30557e45db071c8d55
Alexander Potashev <aspotashev@gmail.com>
2011-05-30 00:46:07 +0400

    add update.sh

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+TEMPLATES="~/kde-ru/xx-numbering/templates"
+TEMPLATES_STABLE="~/kde-ru/xx-numbering/stable-templates"
+
+bash -c "cd $TEMPLATES && git svn fetch && git rebase git-svn"
+bash -c "cd $TEMPLATES_STABLE && git svn fetch && git rebase git-svn"
+
+bash -c "cd stupid-id-filler && ./add-templates-from-repo.rb $TEMPLATES ./ids"
+bash -c "cd stupid-id-filler && ./add-templates-from-repo.rb $TEMPLATES_STABLE ./ids"
+
+bash -c "cd stupid-id-filler/database && ./update-database.rb"
+
+bash -c "cd transition-detector && ./update-database.rb"
+
