f0a95b1e660713de5e06ca50e023b59c1ef4f335
Alexander Potashev <aspotashev@gmail.com>
2011-06-05 22:56:33 +0400

    cleanup

--- tools/iddiff/main.cpp
+++ tools/iddiff/main.cpp
@@ -10,8 +10,6 @@ int main(int argc, char *argv[])
 {
 	assert(argc == 3); // 2 arguments
 
-//	Iddiffer *differ = new Iddiffer();
-//	differ->loadIddiff("1.iddiff");
 	std::cout << Iddiffer::generateIddiffText(
 		new TranslationContent(argv[1]),
 		new TranslationContent(argv[2]));
