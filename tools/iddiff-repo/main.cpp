1bfa6536a291dfc98702a33c29456120d4ca55c0
Alexander Potashev <aspotashev@gmail.com>
2011-06-09 23:12:10 +0400

    iddiff-repo: accept multiple files on the command line

--- tools/iddiff-repo/main.cpp
+++ tools/iddiff-repo/main.cpp
@@ -1,5 +1,6 @@
 
 #include <assert.h>
+#include <stdio.h>
 #include <iostream>
 
 #include <gettextpo-helper/translationcontent.h>
@@ -9,19 +10,28 @@
 
 int main(int argc, char *argv[])
 {
-	assert(argc == 2); // 1 argument
-
-	TranslationContent *new_content = new TranslationContent(argv[1]);
-
 	// For loading blobs by their OIDs
 	GitLoader *git_loader = new GitLoader();
 	git_loader->addRepository("/home/sasha/kde-ru/kde-ru-trunk.git/.git");
 	git_loader->addRepository("/home/sasha/kde-ru/kde-l10n-ru-stable/.git");
 
-	TranslationContent *old_content = git_loader->findOldestByTphash(new_content->calculateTpHash());
-	assert(old_content);
+	for (int i = 1; i < argc; i ++)
+	{
+		TranslationContent *new_content = new TranslationContent(argv[i]);
 
-	std::cout << Iddiffer::generateIddiffText(old_content, new_content);
+		const git_oid *tp_hash = new_content->calculateTpHash();
+		if (!tp_hash) // not a .po file
+			continue;
+
+		TranslationContent *old_content = git_loader->findOldestByTphash(tp_hash);
+		if (!old_content)
+		{
+			printf("Could not find old content for [%s]\n", argv[i]);
+			assert(0);
+		}
+
+		std::cout << Iddiffer::generateIddiffText(old_content, new_content);
+	}
 
 	return 0;
 }
