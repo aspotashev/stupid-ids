f9ceb8f9a38e9bf0deaf6fbf3c2d44ee534aace6
Alexander Potashev <aspotashev@gmail.com>
2011-06-12 17:43:32 +0400

    Add dbuslokalize library

--- tools/dbuslokalize/build.sh
+++ tools/dbuslokalize/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-g++ main.cpp dbuslokalize.cpp -ldbus-glib-1 -lgettextpo_helper -I/usr/include/dbus-1.0 -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -o dbuslokalize
+g++ main.cpp -ldbus-glib-1 -ldbuslokalize -lgettextpo_helper -o dbuslokalize
