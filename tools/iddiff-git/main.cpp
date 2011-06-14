9ebea52031fd6a63e52cee2794b5110032d7a4d6
Alexander Potashev <aspotashev@gmail.com>
2011-06-14 17:34:41 +0400

    Add tools/iddiff-git (for generating iddiff from a Git commit)

---
+++ tools/iddiff-git/main.cpp
@@ -0,0 +1,66 @@
+
+#include <assert.h>
+#include <stdio.h>
+#include <iostream>
+
+#include <gettextpo-helper/translationcontent.h>
+#include <gettextpo-helper/iddiff.h>
+#include <gettextpo-helper/gitloader.h>
+
+
+int main(int argc, char *argv[])
+{
+	assert(argc == 3); // 2 arguments
+
+	Repository *repo = new Repository(argv[1]);
+	const char *input_oid_str = argv[2];
+	repo->readRepositoryCommits();
+
+	git_oid input_oid;
+	assert(git_oid_mkstr(&input_oid, input_oid_str) == GIT_SUCCESS);
+
+	int commit_index = repo->commitIndexByOid(&input_oid);
+	if (commit_index < 0)
+	{
+		printf("No commit with OID %s in the Git repository at %s\n", input_oid_str, repo->gitDir());
+		assert(0);
+	}
+
+	const Commit *commit = repo->commit(commit_index);
+	Iddiffer *merged_diff = new Iddiffer();
+	for (int i = 0; i < commit->nChanges(); i ++)
+	{
+		const CommitFileChange *change = commit->change(i);
+		Iddiffer *diff = new Iddiffer();
+
+		if (change->type() == CommitFileChange::DEL)
+			continue;
+
+		TranslationContent *new_content = new TranslationContent(repo, change->oid2());
+		TranslationContent *old_content = NULL;
+		if (change->type() == CommitFileChange::ADD)
+		{
+			diff->diffAgainstEmpty(new_content);
+		}
+		else if (change->type() == CommitFileChange::MOD)
+		{
+			old_content = new TranslationContent(repo, change->oid1());
+			diff->diffFiles(old_content, new_content);
+		}
+		else assert(0);
+
+		delete new_content;
+		if (old_content)
+			delete old_content;
+
+		merged_diff->merge(diff);
+		delete diff;
+	}
+
+	// TODO: copy commit->time() ("Date:"), author ("Author:") and message ("Subject:") to the header of Iddiff
+
+	std::cout << merged_diff->generateIddiffText();
+
+	return 0;
+}
+
