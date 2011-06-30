9c4d0b8f1fbf932b2fc21a365aa474a62df74ad5
Alexander Potashev <aspotashev@gmail.com>
2011-06-30 22:04:55 +0400

    iddiff-repo: build using CMake; refactor

--- tools/iddiff-repo/main.cpp
+++ tools/iddiff-repo/main.cpp
@@ -9,6 +9,30 @@
 #include <gettextpo-helper/stupids-client.h>
 
 
+void processFile(GitLoader *git_loader, Iddiffer *merged_diff, const char *filename)
+{
+	TranslationContent *new_content = new TranslationContent(filename);
+
+	const git_oid *tp_hash = new_content->calculateTpHash();
+	if (!tp_hash) // not a .po file
+		return;
+	if (stupidsClient.getFirstId(tp_hash) == 0)
+	{
+		printf("Unknown tp_hash. Translation file: [%s]\nThis probably means that someone (probably, translator of this file) has generated the translation template by hand, or has changed something in the template.\n", filename);
+		return;
+	}
+
+	Iddiffer *diff = new Iddiffer();
+	TranslationContent *old_content = git_loader->findOldestByTphash(tp_hash);
+	if (old_content)
+		diff->diffFiles(old_content, new_content);
+	else
+		diff->diffAgainstEmpty(new_content); // diff against /dev/null
+
+	merged_diff->merge(diff);
+	delete diff;
+}
+
 int main(int argc, char *argv[])
 {
 	// For loading blobs by their OIDs
@@ -19,29 +43,7 @@ int main(int argc, char *argv[])
 	Iddiffer *merged_diff = new Iddiffer();
 
 	for (int i = 1; i < argc; i ++)
-	{
-		TranslationContent *new_content = new TranslationContent(argv[i]);
-
-		const git_oid *tp_hash = new_content->calculateTpHash();
-		if (!tp_hash) // not a .po file
-			continue;
-		if (stupidsClient.getFirstId(tp_hash) == 0)
-		{
-			printf("Unknown tp_hash. Translation file: [%s]\nThis probably means that someone (probably, translator of this file) has generated the translation template by hand, or has changed something in the template.\n", argv[i]);
-			continue;
-			//assert(0);
-		}
-
-		Iddiffer *diff = new Iddiffer();
-		TranslationContent *old_content = git_loader->findOldestByTphash(tp_hash);
-		if (old_content)
-			diff->diffFiles(old_content, new_content);
-		else
-			diff->diffAgainstEmpty(new_content); // diff against /dev/null
-
-		merged_diff->merge(diff);
-		delete diff;
-	}
+		processFile(git_loader, merged_diff, argv[i]);
 
 	std::cout << merged_diff->generateIddiffText();
 
