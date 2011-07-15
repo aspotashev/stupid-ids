adbda6b5643c224bddeb277a5438097cb20f253a
Alexander Potashev <aspotashev@gmail.com>
2011-07-15 14:44:11 +0400

    tools/stupids-rerere-trusted-filter: filter against several .iddiff files

--- tools/stupids-rerere-trusted-filter/main.cpp
+++ tools/stupids-rerere-trusted-filter/main.cpp
@@ -6,21 +6,30 @@
 
 int main(int argc, char *argv[])
 {
-	assert(argc == 3); // 2 arguments
-	const char *rerere_path = argv[1];
-	const char *input_path = argv[2];
+	assert(argc >= 3); // at least 2 arguments
+	const char *input_path = argv[1];
 
 	Iddiffer *input_diff = new Iddiffer();
 	if (!input_diff->loadIddiff(input_path)) // input_path does not exist
 		return 1;
 
-	Iddiffer *rerere_diff = new Iddiffer();
-	if (!rerere_diff->loadIddiff(rerere_path)) // rerere not found, cannot filter
-		return 2;
+	for (int i = 2; i < argc; i ++)
+	{
+		const char *rerere_path = argv[i];
 
-	Iddiffer *filtered = new Iddiffer();
-	filtered->filterTrustedIddiff(rerere_diff, input_diff);
-	std::cout << filtered->generateIddiffText();
+		Iddiffer *rerere_diff = new Iddiffer();
+		if (!rerere_diff->loadIddiff(rerere_path)) // rerere not found, cannot filter
+			return 2;
+
+		Iddiffer *filtered = new Iddiffer();
+		filtered->filterTrustedIddiff(rerere_diff, input_diff);
+		delete rerere_diff;
+
+		delete input_diff;
+		input_diff = filtered;
+	}
+
+	std::cout << input_diff->generateIddiffText();
 
 	return 0;
 }
