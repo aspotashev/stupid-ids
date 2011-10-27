6eaf256466b904dba97056413cbb426989aab515
Alexander Potashev <aspotashev@gmail.com>
2011-10-27 14:27:10 +0400

    Add tools/iddiff-minimize-ids

---
+++ tools/iddiff-minimize-ids/main.cpp
@@ -0,0 +1,22 @@
+
+#include <assert.h>
+
+#include <gettextpo-helper/iddiff.h>
+
+
+int main(int argc, char *argv[])
+{
+    assert(argc == 2 || argc == 3); // 1 or 2 arguments
+    const char *src_path = argv[1];
+    const char *dest_path = (argc == 3) ? argv[2] : src_path;
+
+    Iddiffer *src_diff = new Iddiffer();
+    if (!src_diff->loadIddiff(src_path)) // src_path does not exist
+        return 1;
+
+    src_diff->minimizeIds();
+    src_diff->writeToFile(dest_path);
+
+    return 0;
+}
+
