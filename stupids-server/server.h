07ff472556e0929450ba33acce3f671a931a5a37
Alexander Potashev <aspotashev@gmail.com>
2015-10-22 09:45:14 +0300

    Coding style

--- stupids-server/server.h
+++ stupids-server/server.h
@@ -8,67 +8,67 @@ class MappedFileIdMapDb;
 class Server : public TcpCommandServer
 {
 public:
-	class ExceptionTpHashNotFound : public std::exception
-	{
-	public:
-		ExceptionTpHashNotFound(const GitOid &tp_hash);
-		virtual ~ExceptionTpHashNotFound() throw();
+    class ExceptionTpHashNotFound : public std::exception
+    {
+    public:
+        ExceptionTpHashNotFound(const GitOid &tp_hash);
+        virtual ~ExceptionTpHashNotFound() throw();
 
-	private:
-		virtual const char *what() const throw();
+    private:
+        virtual const char *what() const throw();
 
-		GitOid m_tpHash;
-	};
+        GitOid m_tpHash;
+    };
 
 
-	Server();
-	~Server();
+    Server();
+    ~Server();
 
-	virtual void commandHandler();
+    virtual void commandHandler();
 
 private:
-	GitOid recvOid();
-	uint32_t recvLong();
-	std::vector<int> recvLongVector();
+    GitOid recvOid();
+    uint32_t recvLong();
+    std::vector<int> recvLongVector();
 
-	/**
-	* @brief Prepares a 32-bit value for sending to client.
-	*
-	* Conversion to network byte order will be done automatically.
-	*
-	* @param data Integer value to send.
-	**/
-	void sendLong(uint32_t data);
+    /**
+    * @brief Prepares a 32-bit value for sending to client.
+    *
+    * Conversion to network byte order will be done automatically.
+    *
+    * @param data Integer value to send.
+    **/
+    void sendLong(uint32_t data);
 
-	/**
-	* @brief Prepares an array of integers (without their count) for sending to client.
-	*
-	* Conversion to network byte order will be done automatically.
-	*
-	* @param arr Array to send.
-	**/
-	void sendLongArray(std::vector<int> arr);
+    /**
+    * @brief Prepares an array of integers (without their count) for sending to client.
+    *
+    * Conversion to network byte order will be done automatically.
+    *
+    * @param arr Array to send.
+    **/
+    void sendLongArray(std::vector<int> arr);
 
-	/**
-	* @brief Prepares a vector of integers (count + data) for sending to client.
-	*
-	* Conversion to network byte order will be done automatically.
-	*
-	* @param vec Vector to send.
-	**/
-	void sendLongVector(std::vector<int> vec);
+    /**
+    * @brief Prepares a vector of integers (count + data) for sending to client.
+    *
+    * Conversion to network byte order will be done automatically.
+    *
+    * @param vec Vector to send.
+    **/
+    void sendLongVector(std::vector<int> vec);
 
-	std::vector<int> getTphashMinIds(GitOid tp_hash);
-	std::vector<int> getMinIds(std::vector<int> ids);
+    std::vector<int> getTphashMinIds(GitOid tp_hash);
+    std::vector<int> getMinIds(std::vector<int> ids);
 
-	void handleGetMinIdArray();
-	void handleGetFirstId();
-	void handleGetFirstIdMulti();
-	void handleGetMinIds();
-	void handleInvolvedByMinIds();
+    void handleGetMinIdArray();
+    void handleGetFirstId();
+    void handleGetFirstIdMulti();
+    void handleGetMinIds();
+    void handleInvolvedByMinIds();
 
 
-	FiledbFirstIds *m_firstIds;
-	MappedFileIdMapDb *m_idMapDb;
+    FiledbFirstIds *m_firstIds;
+    MappedFileIdMapDb *m_idMapDb;
 };
 
